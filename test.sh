17bd1a22c32e4365a253d9f71f725b5a02e029d2
Kevin Jullien <hi@juke.fr>
2018-08-15 01:45:33 +0200

    no way to cache pulled images sadly

--- test.sh
+++ test.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
 set -e
-while IFS=, read -r col1 col2 col3
+while IFS=, read -r col1
 do
     cd ${col1}
     ./test.sh
     cd ..
-    cd ${col2}
-    ./test.sh
-    cd ..
-    cd ${col3}
-    ./test.sh
-    cd ..
-done <<< "fconvert,foptimize,futils"
+done <<< "fconvert
+foptimize
+futils"
 
 ./futils/cli/fcli --version
 
