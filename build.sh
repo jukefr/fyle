86f68cef86a0353715ee09cfa023c8e0daee0275
Kevin Jullien <hi@juke.fr>
2018-08-14 20:06:40 +0200

    update build script to tag with git release

--- build.sh
+++ build.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
+# I use Git Releases with semantic versioning
+current_version=$(git describe --exact-match --tags $(git log -n1 --pretty='%h'))
 current_dir=$(pwd)
+
+function dockbuild() {
+    docker build -t ${col2}:${current_version} .
+    docker tag ${col2}:${current_version} ${col2}:latest
+}
+
+function dockpush(){
+    docker push ${col2}
+}
+
+function build() {
+    echo "Service $col1 Format $col2"
+    cd ${current_dir}/${col1}
+    dockbuild
+    dockpush
+}
+
 while IFS=, read -r col1 col2
 do
     # the check allows to build a specific service directly for dev
     if [ -z "$1" ]; then
-        echo "$col1|$col2"
-        cd ${current_dir}/${col1}
-        docker build -t ${col2} .
-        docker push ${col2}
+        build
     else
         if [[ ${col1} = *"$1"* ]]; then
-            echo "$col1|$col2"
-            cd ${current_dir}/${col1}
-            docker build -t ${col2} .
-            docker push ${col2}
+            build
         fi
     fi
 
