18ee781af467eb63120615d9b90890e1ef9e03b9
Kevin Jullien <hi@juke.fr>
2018-08-15 00:54:35 +0200

    faster travis testing

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 current_dir=$(pwd)
 
+LATEST_TAG=$(git describe --tags --abbrev=0)
+CURRENT_REVISION=$(git describe)
+NUMBER_FILES_CHANGED=$(git diff --name-only HEAD ${LATEST_TAG} | wc -l)
+# List of files changed since last tagged release (new docker images)
+CHANGES=($(git diff --name-only HEAD ${LATEST_TAG}))
+
 function dockbuild() {
     docker build -t ${col2} .
 }
@@ -13,10 +19,16 @@ function build() {
 
 while IFS=, read -r col1 col2
 do
-    # the check allows to build a specific service directly for dev
+    # if no argument is specified it builds images that have changed since
+    # the last git tag
+    # an argument allows to build a specific service directly for dev
     # $ ./build.sh foptimize/image/png
     if [ -z "$1" ]; then
-        build
+        for i in "${CHANGES[@]}"; do
+            if [[ ${i} = *"$col1"* ]]; then
+                build
+            fi
+        done
     else
         if [[ ${col1} = *"$1"* ]]; then
             build
