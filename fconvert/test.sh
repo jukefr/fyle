766935fec13105d14261b7ee461340200ac10ae5
Kevin Jullien <hi@juke.fr>
2018-08-15 02:21:52 +0200

    add timeout to tests

--- fconvert/test.sh
+++ fconvert/test.sh
@@ -4,7 +4,8 @@ mkdir -p tmp
 cd tmp
 while IFS="," read -r col1 col2 col3 col4; do
     echo "Testing service $col1 format $col2 to $col3 with url $(echo "$col4" | cut -c 1-40)..."
-    docker run -v $(pwd):/d/ fconvert/${col1} "$col4" tmp.${col3}
+    # Conversions can take some time
+    timeout 60 docker run -v $(pwd):/d/ fconvert/${col1} "$col4" tmp.${col3}
 done < ../test.csv
 cd ..
 rm -rf tmp/
--- futils/test.sh
+++ futils/test.sh
@@ -4,7 +4,7 @@ mkdir -p tmp
 cd tmp
 while IFS="," read -r col1 col2 col3; do
     echo "Testing service $col1 format $col2 with url $(echo "$col3" | cut -c 1-40)..."
-    docker run -v $(pwd):/d/ -e size="50%" futils/${col1} "$col3"
+    timeout 30 docker run -v $(pwd):/d/ -e size="50%" futils/${col1} "$col3"
 done < ../test.csv
 cd ..
 rm -rf tmp/
