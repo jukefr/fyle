910758d7c80b3bc4144bc28e9fb2d2e7d2ecc7bc
Kevin Jullien <hi@juke.fr>
2018-08-19 03:39:24 +0200

    should work

--- scripts/test_lint.sh
+++ scripts/test_lint.sh
@@ -4,13 +4,13 @@
 test_lint() {
     mapfile -t TMP_SCRIPTS < <(find "$PWD/$1" -name "*.sh" -print)
     for TMP_SCRIPT in "${TMP_SCRIPTS[@]}"; do
-        TMP_REL_PATH="$(realpath "$TMP_SCRIPT")"
+        TMP_REL_PATH="$(printf '%s\n' "${TMP_SCRIPT//$PWD/}" | cut -c2- )"
         echo "Linting $TMP_REL_PATH"
         docker run --rm -w="/d/" -v "$PWD:/d/" koalaman/shellcheck-alpine /bin/shellcheck -f gcc -x "$TMP_REL_PATH"
     done
     mapfile -t TMP_SCRIPTS < <(find "$PWD/$1" -name "Dockerfile" -print)
     for TMP_SCRIPT in "${TMP_SCRIPTS[@]}"; do
-        TMP_REL_PATH="$(realpath "$TMP_SCRIPT")"
+        TMP_REL_PATH="$(printf '%s\n' "${TMP_SCRIPT//$PWD/}" | cut -c2- )"
         echo "Linting $TMP_REL_PATH"
         docker run --rm -i hadolint/hadolint < "$TMP_REL_PATH"
     done
--- test.sh
+++ test.sh
@@ -34,7 +34,7 @@ init() {
     echo "Linting scripts..."
     mapfile -t TMP_SCRIPTS < <(find "$PWD/scripts/" -name "*.sh" -print)
     for TMP_SCRIPT in "${TMP_SCRIPTS[@]}"; do
-        TMP_REL_PATH="$(realpath "$TMP_SCRIPT")"
+        TMP_REL_PATH="$(printf '%s\n' "${TMP_SCRIPT//$PWD/}" | cut -c2- )"
         echo "Linting $TMP_REL_PATH"
         docker run --rm -w="/d/" -v "$PWD:/d/" koalaman/shellcheck-alpine /bin/shellcheck -f gcc -x "$TMP_REL_PATH"
     done
